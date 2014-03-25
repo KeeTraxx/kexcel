6c1dbf5a1c7001e169ab549f4d6a49e56ffd8f5a
Khôi Tran <kt@compile.ch>
2014-03-25 17:17:50 +0100

    first working

--- test.js
+++ test.js
@@ -4,8 +4,15 @@
 var kexcel = require('./');
 
 var fs = require('fs');
+var Q = require('q');
 
-kexcel.open('export.xlsx').then(function(kexcelfile){
-    console.log('wtf');
-    kexcelfile.getSheets().then(console.log);
-});
+Q.nfcall( kexcel.open, 'export.xlsx')
+    .then(function(kexcel){
+        console.log( kexcel.sheets );
+        console.log( kexcel.sheets[0].getName() );
+        console.log( kexcel.sheets[0].getTree() );
+        kexcel.sheets[0].replaceRow(12, {A12: 'bla'});
+        console.log('a');
+        kexcel.sheets[0].save();
+        console.log('f');
+    });
