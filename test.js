4c6e8edbba7327a210c2b29861fc4092d50fe462
Khôi Tran <kt@compile.ch>
2014-05-01 20:30:14 +0200

    added shared strings support

--- test.js
+++ test.js
@@ -6,10 +6,11 @@ var kexcel = require('./');
 var fs = require('fs');
 var Q = require('q');
 
-Q.nfcall( kexcel.open, 'export.xlsx')
+Q.nfcall( kexcel.open, 'templates/empty.xlsx')
     .then(function(kexcel){
         try{
             var newsheet = kexcel.duplicateSheet(kexcel.sheets[0],'testduplicate');
+            console.log(kexcel.sharedStringsXml.xml.write());
             newsheet.replaceRow(12,{A12:'bla2'});
             kexcel.sheets[0].replaceRow(12, {A12: 'bla'});
             kexcel.sheets[0].replaceRow(13, {B13: 'blamore'});
