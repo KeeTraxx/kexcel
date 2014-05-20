0d9490af47e43dd5b88192da6b43a91d63cf785c
Khôi Tran <kt@compile.ch>
2014-05-20 11:24:56 +0200

    Functionality to delete sheet

--- test.js
+++ test.js
@@ -13,13 +13,16 @@ Q.nfcall( kexcel.open, 'templates/empty.xlsx')
 
             for(var r=1;r<100;r++) {
                 for(var c=1;c<100;c++) {
-                    newsheet.setCellValue( r, c,  String(~~(Math.random() * 300) ));
+                    newsheet.setCellValue( r, c,  ~~(Math.random() * 300) );
                 }
             }
 
             kexcel.sheets[0].setCellValue(13,3,'tester');
             kexcel.sheets[0].setCellValue(13,5,'tester2');
             kexcel.sheets[0].setCellValue(14,3,'tester');
+
+            kexcel.deleteSheetAt(0);
+
             var output = fs.createWriteStream(__dirname + '/tester.xlsx');
             kexcel.pipe(output,function(){
                 console.log('done!');
