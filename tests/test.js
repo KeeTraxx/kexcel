3308f5d9b2108e68137f66cd5de91dcb35559e1f
Khôi Tran <kt@compile.ch>
2014-07-11 09:22:21 +0200

    Added possibility to set formulas of cells using `setCellValue` . fixes #2

--- tests/test.js
+++ tests/test.js
@@ -1,46 +1,48 @@
 var kexcel = require('../');
 var fs = require('fs');
 var path = require('path');
-var filepath = path.join(__dirname, '../examples/input','empty.xlsx');
+var filepath = path.join(__dirname, '../examples/input', 'empty.xlsx');
 
-kexcel.open( filepath, function(err, workbook) {
+kexcel.open(filepath, function (err, workbook) {
     try {
-    // Get first sheet
-    var sheet1 = workbook.sheets[0];
+        // Get first sheet
+        var sheet1 = workbook.sheets[0];
 
-    // Duplicate a sheet
-    var duplicatedSheet = workbook.duplicateSheet(sheet1,'My duplicated sheet');
+        // Duplicate a sheet
+        var duplicatedSheet = workbook.duplicateSheet(sheet1, 'My duplicated sheet');
 
-    // Add some data to the first sheet
-    // Caution!! Row and column are 1-based
-    sheet1.setCellValue(1,1,'foo in first row and first column');
-    sheet1.setCellValue(5,1,'bar in fifth row and first column');
-    sheet1.setCellValue(5,8,'Somewhere...');
-        sheet1.setCellValue(5,26,'Z Column');
-        sheet1.setCellValue(5,27,'AA Column');
-    sheet1.setCellValue(6,1,'This cell copies the style from cell A1', 'A1');
+        // Add some data to the first sheet
+        // Caution!! Row and column are 1-based
+        sheet1.setCellValue(1, 1, 'foo in first row and first column');
+        sheet1.setCellValue(5, 1, 'bar in fifth row and first column');
+        sheet1.setCellValue(5, 8, 'Somewhere...');
+        sheet1.setCellValue(5, 26, 'Z Column');
+        sheet1.setCellValue(5, 27, 'AA Column');
+        sheet1.setCellValue(6, 1, 'This cell copies the style from cell A1', 'A1');
+        sheet1.setCellValue(7,1,'=HYPERLINK("http://www.google.ch","Google")');
+        sheet1.setCellValue(8,1,'=1+1');
 
-    console.log(sheet1.getRowValues(5));
+        console.log(sheet1.getRowValues(5));
 
-    console.log('Should print: Somewhere...',sheet1.getCellValue(5,8));
+        console.log('Should print: Somewhere...', sheet1.getCellValue(5, 8));
 
-    // Put random numbers in the duplicated sheet
-    for(var r=1;r<100;r++) {
-        for(var c=1;c<100;c++) {
-            duplicatedSheet.setCellValue( r, c,  ~~(Math.random() * 300) );
+        // Put random numbers in the duplicated sheet
+        for (var r = 1; r < 100; r++) {
+            for (var c = 1; c < 100; c++) {
+                duplicatedSheet.setCellValue(r, c, ~~(Math.random() * 300));
+            }
         }
-    }
 
-    console.log('Random number: ',duplicatedSheet.getCellValue(5,8));
+        console.log('Random number: ', duplicatedSheet.getCellValue(5, 8));
 
-    // Save the file
-    var output = fs.createWriteStream(__dirname + '/output.xlsx');
-    workbook.pipe(output,function(){
-        console.log('done!');
-        workbook.close();
-    });
+        // Save the file
+        var output = fs.createWriteStream(__dirname + '/output.xlsx');
+        workbook.pipe(output, function () {
+            console.log('done!');
+            workbook.close();
+        });
 
-    } catch(e) {
+    } catch (e) {
         console.log(e.stack);
     }
 });
