cd63af79de492131cf8fc466eca3f1e4d1776f57
Khôi Tran <kt@compile.ch>
2015-01-25 00:08:37 +0100

    WIP

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,9 @@
 var kexcel = require('../');
 var fs = require('fs');
-kexcel.open(__dirname + '/Mappe1.xlsx',function(err, workbook){
-    workbook.pipe(fs.createWriteStream('super.xlsx'));
-});
+/*kexcel.open(__dirname + '/Mappe1.xlsx',function(err, workbook){
+    //workbook.pipe(fs.createWriteStream('super.xlsx'));
+});*/
 
 kexcel.new(function(err, workbook){
-
+    workbook.pipe(fs.createWriteStream('super.xlsx'));
 });
