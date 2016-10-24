d6b143472a758a98e6cc4802712cce6492a64a03
Khôi Tran <tran@puzzle.ch>
2016-10-24 22:59:01 +0200

    Properly find node_modules dir

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,14 +1,20 @@
 const webpack = require('webpack');
 const path = require('path');
+const findup = require('findup-sync');
 
 const ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
 const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
+const MODULES_DIR = findup('node_modules');
+
 module.exports = {
     entry: {kexcel: './ts/Workbook'},
     resolve: {
+        root: [
+            path.join(__dirname, '..', 'node_modules'),
+        ],
         extensions: ['', '.ts', '.json'],
         modules: ['ts', 'node_modules']
     },
@@ -31,16 +37,16 @@ module.exports = {
         new CleanWebpackPlugin(['dist'], {root: path.dirname(module.parent.filename)}),
         new ForkCheckerPlugin(),
         /*new UglifyJsPlugin({
-            beautify: true,
-            mangle: false,
-            compress: false,
-            comments: true,
-            sourceMap: true
-            /*
-            beautify: false, //prod
-            mangle: {screw_ie8: true, keep_fnames: true}, //prod
-            compress: {screw_ie8: true}, //prod
-            comments: false //prod*/
+         beautify: true,
+         mangle: false,
+         compress: false,
+         comments: true,
+         sourceMap: true
+         /*
+         beautify: false, //prod
+         mangle: {screw_ie8: true, keep_fnames: true}, //prod
+         compress: {screw_ie8: true}, //prod
+         comments: false //prod*/
         //})
     ],
     debug: false,
@@ -48,8 +54,8 @@ module.exports = {
     output: {
         path: 'dist',
         /*filename: '[name].[chunkhash].bundle.js',
-        sourceMapFilename: '[name].[chunkhash].bundle.map',
-        chunkFilename: '[id].[chunkhash].chunk.js'*/
+         sourceMapFilename: '[name].[chunkhash].bundle.map',
+         chunkFilename: '[id].[chunkhash].chunk.js'*/
         filename: '[name].bundle.js',
         sourceMapFilename: '[name].bundle.map',
         chunkFilename: '[id].[chunkhash].chunk.js',
@@ -58,7 +64,7 @@ module.exports = {
         devtoolModuleFilenameTemplate: 'kexcel/[resource-path]'
     },
     externals: [
-        nodeExternals()
+        nodeExternals({modulesDir: MODULES_DIR})
     ],
     node: {
         __dirname: true
