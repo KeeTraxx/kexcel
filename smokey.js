aa63207884d03581dc162ed48251affbfbb3172c
Khôi Tran <tran@puzzle.ch>
2016-10-24 22:00:58 +0200

    Bumped to 3.1.1

--- smokey.js
+++
@@ -1,8 +0,0 @@
-const kexcel = require('./dist/kexcel.bundle');
-const fs = require('fs');
-
-kexcel.new().then(function(wb){
-   console.log(wb);
-   wb.getSheet(0).setCellValue(1,1,'test');
-   wb.pipe(fs.createWriteStream('test.xlsx'));
-});
